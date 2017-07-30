61439df536fe8abe83a194208537410addf42faa
narges-rzv <narges.sharif@gmail.com>
2017-07-30 16:13:01 -0400

    added monthly&plotting

--- src/main.py
+++ src/main.py
@@ -3,8 +3,16 @@ import csv
 from dateutil import parser
 import numpy as np
 import matplotlib.pylab as plt
+from urllib.request import Request, urlopen
 
-datadic = {}
+import urllib
+
+'''Doesn't work yet :( '''
+def download_historical_data(stname='amzn', dir1='../data/'):
+	stock_url = 'https://query1.finance.yahoo.com/v7/finance/download/'+stname+'?period1=1343620800&period2=1501387200&interval=1d&events=history&crumb=xBC5oaavHyP'
+	print(stock_url)
+	fname=dir1+stname+'.csv'
+	response = urlopen(stock_url)
 
 def load_data(dir1='../data/'):
 	filenames = listdir(dir1)
@@ -21,7 +29,7 @@ def day_of_week(datadic):
 	for key1 in datadic:
 		weekdayssum = np.zeros((7, len(datadic[key1])), dtype=float)
 		for ix, d in enumerate(datadic[key1]):
-			weekdayssum[d.weekday(), ix ] = datadic[key1][d][0]
+			weekdayssum[d.weekday(), ix ] = datadic[key1][d][3]
 		cnt = (weekdayssum != 0).sum(axis=1).ravel()
 		weekdayssum[weekdayssum==0] = np.nan
 		avgweekday = np.nanmean(weekdayssum, axis=1)
@@ -30,6 +38,59 @@ def day_of_week(datadic):
 		high = avgweekday + (1.96/np.sqrt(cnt)) * stdweekday
 		print('average opening for ', key1, ' is:', [('{0:4.3f}'.format(avgweekday[i]) + ' [' + '{0:4.3f}'.format(low[i]) + ' ' + '{0:4.3f}'.format(high[i]) + ']') for i in range(0,5)])
 		plot_data[key1] = avgweekday, stdweekday
-	return plot_data
+	labels = ['mon','tue','wed','thu','fri','sat','sun']
+	return plot_data, labels
 
+def month_of_year(datadic):
+	plot_data = {}
+	for key1 in datadic:
+		monthssum = np.zeros((12, len(datadic[key1])), dtype=float)
+		for ix, d in enumerate(datadic[key1]):
+			monthssum[d.month - 1, ix] = datadic[key1][d][0]
+		cnt = (monthssum != 0).sum(axis=1).ravel()
+		monthssum[monthssum==0] = np.nan
+		avgmonth = np.nanmean(monthssum, axis=1)
+		stdmonth = np.nanstd(monthssum, axis=1)
+		low = avgmonth - (1.96/np.sqrt(cnt)) * stdmonth
+		high = avgmonth + (1.96/np.sqrt(cnt)) * stdmonth
+		print('average opening for ', key1, ' is:', [('{0:4.3f}'.format(avgmonth[i]) + ' [' + '{0:4.3f}'.format(low[i]) + ' ' + '{0:4.3f}'.format(high[i]) + ']') for i in range(0,12)])
+		plot_data[key1] = avgmonth, (1.96/np.sqrt(cnt)) * stdmonth
+	labels = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+	return plot_data, labels
+
+def biweekly_of_year(datadic):
+	plot_data = {}
+	for key1 in datadic:
+		monthssum = np.zeros((12*2, len(datadic[key1])), dtype=float)
+		for ix, d in enumerate(datadic[key1]):
+			month = d.month - 1
+			secondhalf = 0
+			day = d.day - 1; 
+			if (day >=15) & (day >= 15 + (7 - d.weekday())):
+				secondhalf = 1
+			monthssum[ month*2 + secondhalf, ix] = datadic[key1][d][0]
+		cnt = (monthssum != 0).sum(axis=1).ravel()
+		monthssum[monthssum==0] = np.nan
+		avgmonth = np.nanmean(monthssum, axis=1)
+		stdmonth = np.nanstd(monthssum, axis=1)
+		low = avgmonth - (1.96/np.sqrt(cnt)) * stdmonth
+		high = avgmonth + (1.96/np.sqrt(cnt)) * stdmonth
+		print('average opening for ', key1, ' is:', [('{0:4.3f}'.format(avgmonth[i]) + ' [' + '{0:4.3f}'.format(low[i]) + ' ' + '{0:4.3f}'.format(high[i]) + ']') for i in range(0,12)])
+		plot_data[key1] = avgmonth, (1.96/np.sqrt(cnt)) * stdmonth
+	labels = ['jan12','jan34','feb12','feb34','mar12','mar34','apr12','apr34','may12','may34','jun12','jun34','jul12','jul34','aug12','aug34','sep12','sep34','oct12','oct34','nov12','nov34','dec12','dec34']
+	return plot_data, labels
+
+
+def plot_data(plot_data, labels):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	pltlist = []
+	width=0.1
+	for ix, k in enumerate(plot_data):
+		pltlist.append(ax.bar(np.arange(0,len(labels))-width*(ix),[i for i in plot_data[k][0]], width=width, yerr=[i for i in plot_data[k][1]]))
+	ax.set_ylabel('average stock opening')
+	ax.set_xticks(np.arange(0,len(labels))-width)
+	ax.set_xticklabels(labels)
+	ax.legend( [axxi[0] for axxi in pltlist], list(plot_data.keys()) )
+	plt.show()
 
